8297d9b4663e755c120ce2c52b0ba337de639378
Lila Leatherman <lila.leatherman@gmail.com>
2024-02-07 10:33:21 -0700

    Revert "Update yai.R - add predict.all = TRUE to RF"
    This reverts commit 9ecde69cf84d2454d2737aa96eeba90279a6337d.

--- R/yai.R
+++ R/yai.R
@@ -578,7 +578,7 @@ yai <- function(x=NULL,y=NULL,data=NULL,k=1,noTrgs=FALSE,noRefs=FALSE,
          mt = if (is.null(mtry)) max(floor(sqrt(ncol(xRefs))),1) else 
                                  min(mtry, ncol(xRefs))
          ranForest=randomForest(x=xRefs,y=yone,proximity=FALSE,importance=TRUE,
-                                keep.forest=TRUE,mtry=mt,ntree=ntree, keep.inbag=TRUE, predict.all = TRUE)
+                                keep.forest=TRUE,mtry=mt,ntree=ntree, keep.inbag=TRUE)
          ranForest$type="yaImputeUnsupervised"
          ranForest=list(unsupervised=ranForest)
       }
@@ -624,7 +624,7 @@ yai <- function(x=NULL,y=NULL,data=NULL,k=1,noTrgs=FALSE,noRefs=FALSE,
                                     min(mtry, length(xN))
             ranForest[[i]]=randomForest(x=xRefs[,xN,FALSE],
               y=yone,proximity=FALSE,importance=TRUE,keep.forest=TRUE,
-              mtry=mt,ntree=ntree[i], keep.inbag = TRUE, predict.all = TRUE)
+              mtry=mt,ntree=ntree[i], keep.inbag = TRUE)
          }
          names(ranForest)=colnames(yRefs)
       }
